8cb22b51969eebebb19e160b1c92a984adb8cb6c
fadhlihilmans <fadhli.hilmanss@gmail.com>
2026-04-26 22:27:46 +0700

    p

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+        <nav>
+        <div className="logo">
+            <a href="">LOGO</a>
+        </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
